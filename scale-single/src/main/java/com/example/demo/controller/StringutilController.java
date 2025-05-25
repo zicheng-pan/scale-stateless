25a21c2b0b62d01059ff51ed58fcf4c481abfde5
Zicheng Pan <zpan2@jaguarlandrover.com>
2025-05-25 18:04:36 +0800

    add redis

--- scale-single/src/main/java/com/example/demo/controller/StringutilController.java
+++ scale-single/src/main/java/com/example/demo/controller/StringutilController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,15 @@ public class StringutilController {
 
   private final AtomicInteger requestCount = new AtomicInteger(0);
 
+  @Autowired
+  private StringRedisTemplate stringRedisTemplate;
+
+
   @GetMapping("/toUpper")
   public String add(@RequestParam String str) throws InterruptedException {
+//    引入redis
+    stringRedisTemplate.opsForValue().increment("requests-count");
+    
     String appName = System.getenv("APP_NAME");
     requestCount.incrementAndGet();
     Thread.sleep(2000);
@@ -25,7 +34,10 @@ public class StringutilController {
 
   @GetMapping("/reqeusts-count")
   public int getRequestCount() {
-    return this.requestCount.get();
+//    使用redis中的内容
+//    stringRedisTemplate.opsForValue().increment("requests-count");
+    return Integer.parseInt(stringRedisTemplate.opsForValue().get("requests-count"));
+//    return this.requestCount.get();
   }
 
 }
