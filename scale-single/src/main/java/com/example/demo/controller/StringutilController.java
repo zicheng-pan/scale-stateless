d986155c7cbc1a6ac8eb56b135c3da159b7a2f02
Zicheng Pan <zpan2@jaguarlandrover.com>
2025-05-27 11:20:21 +0800

    删除掉redis

--- scale-single/src/main/java/com/example/demo/controller/StringutilController.java
+++ scale-single/src/main/java/com/example/demo/controller/StringutilController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +15,8 @@ public class StringutilController {
 
   private final AtomicInteger requestCount = new AtomicInteger(0);
 
-  @Autowired
-  private StringRedisTemplate stringRedisTemplate;
-
-
   @GetMapping("/toUpper")
   public String add(@RequestParam String str) throws InterruptedException {
-//    引入redis
-//    stringRedisTemplate.opsForValue().increment("requests-count");
-
     String appName = System.getenv("APP_NAME");
     requestCount.incrementAndGet();
     Thread.sleep(2000);
@@ -34,9 +25,6 @@ public class StringutilController {
 
   @GetMapping("/reqeusts-count")
   public int getRequestCount() {
-//    return Integer.parseInt(stringRedisTemplate.opsForValue().get("requests-count") == null ? "0" :
-//        Objects.requireNonNull(stringRedisTemplate.opsForValue().get("requests-count")));
-
     return this.requestCount.get();
   }
 
